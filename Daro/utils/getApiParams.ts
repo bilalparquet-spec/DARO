86ca624d84860a5a214ef330ca20315a7322e1ae
bilalparquet-spec <bilalparquet@gmail.com>
2026-08-23 02:57:59 +0100

    Delete Daro/utils directory

--- Daro/utils/getApiParams.ts
+++
@@ -1,6 +0,0 @@
-import { ExpoRequest } from 'expo-router/server';
-import qs from 'qs';
-
-export default function getApiParams(request: ExpoRequest) {
-  return qs.parse(request.expoUrl.searchParams.toString()) as { [key: string]: string };
-}
