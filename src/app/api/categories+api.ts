aadf2e56b3e6f7a4742cedb4a804f88f9a7e6d7c
bilalparquet-spec <bilalparquet@gmail.com>
2026-08-23 02:53:16 +0100

    Delete src directory

--- src/app/api/categories+api.ts
+++
@@ -1,8 +0,0 @@
-import { ExpoResponse } from 'expo-router/server';
-
-import prisma from '@/server/db';
-
-export async function GET() {
-  const categoryList = await prisma.category.findMany();
-  return ExpoResponse.json(categoryList);
-}
--- src/components/common/menu/index.tsx
+++
@@ -1,2 +0,0 @@
-export { default as MenuList } from './List';
-export { default as MenuItem } from './Item';
